6a0bd3d371053052a7513c1f896795f60fdd7886
fast-facts <107660271+fast-facts@users.noreply.github.com>
2024-08-01 08:25:35 -0400

    chore: Fix type import

--- src/github.service.ts
+++ src/github.service.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import { getOctokit } from '@actions/github';
+import type { getOctokit } from '@actions/github';
 import { Context } from '@actions/github/lib/context';
 
 export class GithubService {
